30571cd992184fadc3f44fcb3435f4c7809ba31e
Alan Tai <ayltai@gmail.com>
2016-04-13 17:29:27 +0800

    Fix issue row layout

--- plusOneCounter.js
+++ plusOneCounter.js
@@ -18,13 +18,17 @@ $('.js-issue-row').each(function() {
       }
     });
     
+    var issueTitleContainer = issueRow.find('.table-list-cell.issue-title');
+    var commentsContainer   = issueRow.find('.issue-comments');
+    
+    issueTitleContainer.width('720px');
+    commentsContainer.width('80px');
+    
     if (thumbsUpCount > 0) {
-      var thumbsUpIcon      = '<img class="emoji" title=":+1:" alt=":+1:" src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f44d.png" height="20" width="20" align="absmiddle">';
-      var commentsContainer = issueRow.find('.issue-comments');
-      var messageContainer  = commentsContainer.find('.muted-link');
-      var messageCount      = messageContainer.text();
+      var thumbsUpIcon     = '<img class="emoji" title=":+1:" alt=":+1:" src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f44d.png" height="20" width="20" align="absmiddle">';
+      var messageContainer = commentsContainer.find('.muted-link');
+      var messageCount     = messageContainer.text();
       
-      commentsContainer.width("80px");
       commentsContainer.prepend('<a href="' + issueUrl + '" class="muted-link">' + thumbsUpIcon + '&nbsp;' + thumbsUpCount + '</a>&nbsp;&nbsp;');
       messageContainer.contents().last().replaceWith('&nbsp;' + (messageCount - thumbsUpCount));
     }
