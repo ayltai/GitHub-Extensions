394c17059173bf789c476ce11ec3c61a73f995a8
Alan Tai <ayltai@gmail.com>
2016-04-13 17:52:18 +0800

    Show authors who approved PR as hover tip

--- plusOneCounter.js
+++ plusOneCounter.js
@@ -4,18 +4,25 @@ $('.js-issue-row').each(function() {
   
   $.get('https://github.com' + issueUrl, function(data) {
     var thumbsUpCount = 0;
+    var usernames     = [];
     var html          = $.parseHTML(data);
     
-    $(html).find('.comment-body.js-comment-body').each(function() {
-      var commentRow  = $(this);
-      var messageText = commentRow.text().trim();
+    $(html).find('.comment.js-comment').each(function() {
+      var commentContainer = $(this);
+      var username         = commentContainer.find('a.author').text();
       
-      if (messageText == '+1'
-      || messageText == 'lg2m'
-      || commentRow.find('img[title=":+1:"]').length == 1
-      || commentRow.find('img[title=":thumbsup:"]').length == 1) {
-        thumbsUpCount++;
-      }
+      commentContainer.find('.comment-body.js-comment-body').each(function() {
+        var commentRow  = $(this);
+        var messageText = commentRow.text().trim();
+        
+        if (messageText == '+1'
+        || messageText == 'lg2m'
+        || commentRow.find('img[title=":+1:"]').length == 1
+        || commentRow.find('img[title=":thumbsup:"]').length == 1) {
+          usernames.push(username);
+          thumbsUpCount++;
+        }
+      });
     });
     
     var issueTitleContainer = issueRow.find('.table-list-cell.issue-title');
@@ -26,10 +33,21 @@ $('.js-issue-row').each(function() {
     
     if (thumbsUpCount > 0) {
       var thumbsUpIcon     = '<img class="emoji" title=":+1:" alt=":+1:" src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f44d.png" height="20" width="20" align="absmiddle">';
+      var linkTitle        = '';
       var messageContainer = commentsContainer.find('.muted-link');
       var messageCount     = messageContainer.text();
       
-      commentsContainer.prepend('<a href="' + issueUrl + '" class="muted-link">' + thumbsUpIcon + '&nbsp;' + thumbsUpCount + '</a>&nbsp;&nbsp;');
+      for (var i = 0; i < usernames.length; i++) {
+        if (i === 0) {
+          linkTitle = usernames[0];
+        } else {
+          linkTitle += ', ' + usernames[i];
+        }
+      }
+      
+      linkTitle += (usernames.length == 1 ? ' has' : ' have') + ' approved this pull request';
+      
+      commentsContainer.prepend('<a href="' + issueUrl + '" class="muted-link" title="' + linkTitle + '">' + thumbsUpIcon + '&nbsp;' + thumbsUpCount + '</a>&nbsp;&nbsp;');
       messageContainer.contents().last().replaceWith('&nbsp;' + (messageCount - thumbsUpCount));
     }
   });
